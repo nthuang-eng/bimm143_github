6316278818bedbcc97595e81c22bda200c1c0124
Nathalie Huang <nathaliehuang@Nathalies-MacBook-Air.local>
2026-03-13 12:11:32 -0700

    Add class04

---
+++ class04/class04.R
@@ -0,0 +1,9 @@
+#' ---
+#' title: "Class 4 Intro to R"
+#' author: "Nathalie Huang"
+#' #' date: "January 16, 2026"
+#' ---
+
+x <- 1:50
+plot(x)
+plot(x, sin(x), typ="l", col="purple", lwd=3, xlab="Is it lunch time yet?")
